1d270dd7420b53af639992833c21f30230fafb56
Simona Pizio <simona.pizio@gmail.com>
2023-04-13 10:44:25 +0200

    Update dependencies to run app

--- src/components/NewTopicForm.js
+++ src/components/NewTopicForm.js
@@ -1,11 +1,11 @@
 import React, { useState } from "react";
 import { useHistory } from "react-router-dom";
-import { useDispatch, useSelector } from 'react-redux';
+import { useDispatch} from 'react-redux';
 
 import { v4 as uuidv4 } from "uuid";
 import ROUTES from "../app/routes";
 import { ALL_ICONS } from "../data/icons";
-import { addTopic, selectTopics } from "../features/topics/topicsSlice";
+import { addTopic } from "../features/topics/topicsSlice";
 
 export default function NewTopicForm() {
   const [name, setName] = useState("");
